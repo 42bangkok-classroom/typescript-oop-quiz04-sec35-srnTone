ec6c042943b753e1883c8efbda76245d8d6a9d4c
srnTone <68011813@kmitl.ac.th>
2026-03-02 10:01:24 +0700

    q2.1

--- src/ีuser/user.controller.ts
+++ src/ีuser/user.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller()
+@Controller('users/test')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('users')
+  @Get()
   test(): string {
-    return '[]';
+    return this.userService.test();
   }
 }
